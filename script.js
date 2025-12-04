5fdbfa1a13e4761ab6e9fd844b37fe86ee535f5d
krys <krys.michaelis@yahoo.com>
2025-12-04 12:24:41 -0600

    mod 6 changes

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-//we are going to make an event listener that will trigger when the DOM is loaded (aka visiting webpage)
-addEventListener("DOMContentLoaded",  async function(){
-    const response = await this.fetch("http://localhost:3000/api/songs")
-    const songs = await response.json()
+// we are going to make an event listener that will trigger when the DOM is loaded (aka visiting webpage)
+window.addEventListener("DOMContentLoaded", async function () {
+    const response = await fetch("http://localhost:3000/api/songs");
+    const songs = await response.json();
 
-    let html = ""
-    for (let song of songs){
-        html=+`<li>${song.title} - ${song.artst}</li>`
+    let html = "";
+    for (let song of songs) {
+        html += `<li>${song.title} - ${song.artist}</li>`;
     }
 
-    document.querySelector("#addedsong").innerHTML = html
-})
+    document.querySelector("#addedsong").innerHTML = html;
+});
